44193260d39131e6a9623c00b9301fca433b6dd5
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2025-07-19 20:35:19 +0530

    add: a prompt for tags translation

--- src/schemas/translator_schemas.py
+++ src/schemas/translator_schemas.py
@@ -1,10 +1,11 @@
 import datetime
 from pydantic import BaseModel, Field
+from mixins import DBFuncMixin
 
 
 
 
-class TranslatorMetadata(BaseModel):
+class TranslatorMetadata(BaseModel, DBFuncMixin):
     provider_name: str = Field(description="The name of the api provider through which this translation is taking place")
     model_name: str = Field(description="The name of the model used for this translation")
     start_time: datetime.datetime = Field(description="The start timestamp of translation") 
@@ -12,9 +13,7 @@ class TranslatorMetadata(BaseModel):
     total_time_taken: datetime.timedelta = Field(description="The total time taken for this translation")
     input_tokens: int = Field(description="The number of tokens used for this translation")
     output_tokens: int = Field(description="The number of tokens used for this translation")
-    translation_type: str = Field(description="The type of translation that is taking place")
-    prompt_version: str = Field(description="Each prompt will have a version associated with it. This is the version of the prompt that was used for this translation")
+    prompt_version: str = Field(description="Each prompt will have a version associated with it. It can be found in the prompt yaml file")
+    prompt_name: str = Field(description="The name of the prompt that was used for this translation. This field can be found in the yaml prompt file")
     
     
-class TranslatorOutput(BaseModel):
-    translator_metadata: TranslatorMetadata = Field(description="The metadata for this translation")
