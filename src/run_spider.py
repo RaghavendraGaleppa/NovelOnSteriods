1cd046ec09ffd22e8afd62978fbb47bc9364d9ff
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2025-07-22 03:32:03 +0530

    change: rename the run spider function and change its contents

--- src/run_spider.py
+++ src/run_spider.py
@@ -1,26 +1,28 @@
+from typing import Optional
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from src.scraping.scrape_novel import Scrape1qxs
 
+class ItemCollectorPipeline:
+
+    def __init__(self):
+        self.items = []
+
+    def process_item(self, item, spider):
+        self.items.append(item)
+        return item
 
 
 
-def run_my_spider():
-    settings = get_project_settings()
-    scraped_items = []
-    class ItemCollectorPipeline:
-
-        def __init__(self):
-            self.items = scraped_items
-
-        def process_item(self, item, spider):
-            self.items.append(item)
-            return item
-
-    settings.set('ITEM_PIPELINES', {
-        ItemCollectorPipeline: 300
-    })
+def get_pipeline_results(max_pages: int = 100, settings: Optional[dict] = None):
+    if settings is None:
+        settings = {}
+    pipeline = ItemCollectorPipeline()
+    settings['ITEM_PIPELINES'] = {
+        pipeline: 1
+    }
     process = CrawlerProcess(settings)
-    process.crawl(Scrape1qxs)
+    process.crawl(Scrape1qxs, max_pages=max_pages)
     process.start()
-    return scraped_items
+    results = pipeline.items
+    return results
