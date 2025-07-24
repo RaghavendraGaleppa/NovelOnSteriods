f5fc55e2cb329c535eab140933d9a77a2f6d9e64
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2025-07-24 09:43:21 +0530

    add: exceptions for LLM responses add: A Translator class and functions to setup and call llm providers

---
+++ nos/exceptions/translator_exceptions.py
@@ -0,0 +1,22 @@
+from nos.schemas.secrets_schema import Provider
+
+class LLMNoResponseError(Exception):
+    """
+    This exception is raised when the llm does not return a response
+    """
+    def __init__(self, provider: Provider, model_idx: int):
+        self.error_message = f"No response from provider: {provider}, model: {provider.model_names[model_idx]}"
+
+    def __str__(self):
+        return self.error_message
+
+
+class LLMNoUsageError(Exception):
+    """
+    This exception is raised when the llm does not return usage information
+    """
+    def __init__(self, provider: Provider, model_idx: int):
+        self.error_message = f"No usage information from provider: {provider}, model: {provider.model_names[model_idx]}"
+
+    def __str__(self):
+        return self.error_message
--- nos/translators/models.py
+++ nos/translators/models.py
@@ -1,30 +1,61 @@
 from openai import OpenAI
-from schemas.secrets_schema import Provider
-from typing import List
+from typing import List, Optional
 
-
-
+from nos.config import logger
+from nos.schemas.secrets_schema import Provider
+from nos.exceptions.translator_exceptions import LLMNoResponseError, LLMNoUsageError
 
 
 class Translator:
+
     def __init__(self, providers: List[Provider]):
+        """ Setup the translator """
         self.providers = providers
-        self.provider_idx = 0
-        
-    def switch_to_next_provider(self):
-        # Rotate from 0 to maxlen of providers
-        self.provider_idx = (self.provider_idx + 1) % len(self.providers)
-        self.setup_openai_client(self.providers[self.provider_idx])
+        self.provider_idx = 0  # We'll start from the beginning
+        self.model_idx = 0 # First model in the list
+        self.setup_client()
+    
+    def setup_client(self, provider: Optional[Provider]=None):
+        """ Use the internal provider_idx if the provider arg is None"""
+        if provider is None:
+            provider = self.providers[self.provider_idx]
 
-    def setup_openai_client(self, provider: Provider):
-        """ For the given provider, setup an OpenAI client"""
-        self.client = OpenAI(api_key=provider.key)
+        self.client = OpenAI(base_url=provider.url, api_key=provider.key)
+        self.current_provider = provider
+        self.model_idx = 0
+        logger.info(f"Done Setting up client for provider: {provider.provider}, name: {provider.name}")
 
-    def translate(self, text: str, prompt_path: str):
-        pass
-        
 
-    def run_translation_pipeline(self, input_text: str, translation_type: str="chapter", provider_name_preference: str="openai", model_name_preference: str="deepseek/deepseek-chat-v3-0324:free"):
-        """ Go through each provider and each model and try to translate text via each provider """
-        pass
+    def call_provider(self, text: str, system_prompt: Optional[str]=None, temperature: float=0.1, max_tokens: int=2048, raise_usage_error: bool=True):
+        """ Send the text to llm and return response """
+        model_name = self.current_provider.model_names[self.model_idx]
+        logger.debug(f"Calling provider: {self.current_provider.provider}, model: {model_name}")
+
+        messages = []
+        if system_prompt:
+            messages.append({"role": "system", "content": system_prompt})
+        messages.append({"role": "user", "content": text})
+
+
+        response = self.client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+            temperature=temperature,
+            max_tokens=max_tokens
+        )
+
+        if response.choices:
+            response_content = response.choices[0].message.content
+            if response.usage:
+                input_tokens = response.usage.prompt_tokens
+                output_tokens = response.usage.completion_tokens
+            else:
+                if raise_usage_error:
+                    raise LLMNoUsageError(self.current_provider, self.model_idx)
+                else:
+                    input_tokens = None
+                    output_tokens = None
+
+            return response_content, input_tokens, output_tokens
         
+        raise LLMNoResponseError(self.current_provider, self.model_idx)
